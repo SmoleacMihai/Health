923001c122e075644964deccfa125490958191d1
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2021-12-30 00:12:31 +0200

    added js for display none for every div from random page

---
+++ assets/js/randomAdvice.js
@@ -0,0 +1,12 @@
+function getRandomNumberBetween(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+let x = getRandomNumberBetween(1,10)
+
+for (var i = 0; i < 9; i++) {
+    cleaner = document.getElementById("content"+i)
+    cleaner.style.display = "none";
+ }
+function divDisplay(){
+    let 
+}
