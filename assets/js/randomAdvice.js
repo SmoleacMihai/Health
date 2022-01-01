fd76787940406f5aa1f39b165078aea77a150a8f
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-01 21:25:47 +0200

    added functional to js file

--- assets/js/randomAdvice.js
+++ assets/js/randomAdvice.js
@@ -1,10 +1,27 @@
 function getRandomNumberBetween(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
-let x = getRandomNumberBetween(1,10)
 
-// for (var i = 0; i < 11; i++) {
-//     cleaner = document.getElementById("content"+i)
-//     cleaner.style.display = "none";
-//  }
+for (var i = 0; i < 11; i++) {
+    cleaner = document.getElementById("content"+i)
+    cleaner.style.display = "none";
+ }
 
+let usedRandom = 1
+let curentRandom = 0
+
+ function unhider(){
+    let clear = document.getElementById("content0")
+    clear.style.display = "none"
+    curentRandom = getRandomNumberBetween(1,10)
+    while(curentRandom == usedRandom){
+        curentRandom = getRandomNumberBetween(1,10)
+        //console.log("There was a identical number")
+    }
+    element = document.getElementById("content"+curentRandom)
+    element.style.display = "block";
+    // console.log("Function did something")
+    // console.log(curentRandom)
+    usedRandom = curentRandom
+ }
+ document.getElementById("randomButton").onclick = unhider() 
